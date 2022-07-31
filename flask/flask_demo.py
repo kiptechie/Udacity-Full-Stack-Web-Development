ddfec919c5e1ec1400aced300a8fa92185ce125a
Timothy Serem <theecodepoet@gmail.com>
2022-07-31 22:00:02 +0300

    added data dynamically

--- flask/flask_demo.py
+++ flask/flask_demo.py
@@ -22,11 +22,21 @@ class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
 
+    def __repr__(self):
+        return f'<Person id: {self.id}, name: {self.name}>'
+
 
 db.create_all()
 
 
+def addMulamwa():
+    mulamwa = Person(name='Mulamwa')
+    db.session.add(mulamwa)
+    db.session.commit()
+
+
 @app.route('/')
 def index():
+    addMulamwa()
     person = Person.query.first()
     return f'Good Evening {person.name}!'
