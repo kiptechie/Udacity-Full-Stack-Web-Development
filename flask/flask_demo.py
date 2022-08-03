010747349b6e916c6130748da6fd99648773447e
Timothy Serem <theecodepoet@gmail.com>
2022-08-03 19:55:53 +0300

    Migrations

--- flask/flask_demo.py
+++ flask/flask_demo.py
@@ -1,7 +1,9 @@
+from email.policy import default
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
+from flask_migrate import Migrate
 
 load_dotenv()
 
@@ -15,18 +17,20 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{dbUser}:{dbPassword}@{dbHost}:{dbPort}/{dbName}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class Person(db.Model):
     __tablename__ = 'people'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
+    gender = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Person id: {self.id}, name: {self.name}>'
 
 
-db.create_all()
+# db.create_all()
 
 
 def addMulamwa():
