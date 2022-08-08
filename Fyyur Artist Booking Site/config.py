27109eaed49a2a4b543cc0f0584472873630e9e3
Timothy Serem <theecodepoet@gmail.com>
2022-08-08 09:43:34 +0300

    Update config.py

--- Fyyur Artist Booking Site/config.py
+++ Fyyur Artist Booking Site/config.py
@@ -1,4 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+dbUser = os.getenv("DB_USER")
+dbPassword = os.getenv("DB_PASSWORD")
+dbHost = os.getenv("DB_HOST")
+dbPort = os.getenv("DB_PORT")
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -8,6 +16,6 @@ DEBUG = True
 
 # Connect to the database
 
-
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = f'postgresql://{dbUser}:{dbPassword}@{dbHost}:{dbPort}/fyyur'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
